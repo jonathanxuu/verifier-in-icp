6ff583300ec06839992dd0e4bdcbdf6a75a0266c
J <1410101208@qq.com>
2024-01-26 13:22:31 +0800

    remove

--- send_http_get/src/send_http_get_backend/src/lib.rs
+++ send_http_get/src/send_http_get_backend/src/lib.rs
@@ -1,6 +1,6 @@
 use ic_cdk_macros::{self, query, update};
 
-use miden_vm::verify_zk_bool;
+// use miden_vm::verify_zk_bool;
 use std::str;
 
 use ic_cdk::export::{
@@ -253,37 +253,37 @@ pub fn always_fail(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
     Err(getrandom::Error::UNSUPPORTED)
 }
 
-#[ic_cdk::update]
-async fn zk_verify(
-    program_hash: String,
-    public_input: String,
-    proof: String,
-) -> (String, String, Vec<String>) {
-    let modified_proof = proof.replace('\'', "\"");
+// #[ic_cdk::update]
+// async fn zk_verify(
+//     program_hash: String,
+//     public_input: String,
+//     proof: String,
+// ) -> (String, String, Vec<String>) {
+//     let modified_proof = proof.replace('\'', "\"");
 
-    let (zk_verify_result, output) = verify_zk_bool(
-        program_hash.clone(),
-        public_input.clone(),
-        modified_proof.clone(),
-    );
+//     let (zk_verify_result, output) = verify_zk_bool(
+//         program_hash.clone(),
+//         public_input.clone(),
+//         modified_proof.clone(),
+//     );
 
-    if (zk_verify_result == false) {
-        return (
-            "Verification failed".to_string(),
-            "".to_string(),
-            Vec::new(),
-        );
-    } else {
-        let public_input_hash = hex::encode(sha256(&public_input));
-        let origin_message = program_hash + &public_input_hash + &output.join("");
-        let signature = sign(origin_message).await.unwrap();
-        // let signature_hex = signature.unwrap().signature_hex;
-        // let public_key_string = public_key().await.unwrap().public_key_hex;
-        // let verify_result: Result<SignatureVerificationReply, String> = verify(signature_hex.clone(), program_hash.to_string(), public_key_string.clone()).await;
-        // sig, publickey, message
-        return (signature.signature_hex, public_input_hash, output);
-    }
-}
+//     if (zk_verify_result == false) {
+//         return (
+//             "Verification failed".to_string(),
+//             "".to_string(),
+//             Vec::new(),
+//         );
+//     } else {
+//         let public_input_hash = hex::encode(sha256(&public_input));
+//         let origin_message = program_hash + &public_input_hash + &output.join("");
+//         let signature = sign(origin_message).await.unwrap();
+//         // let signature_hex = signature.unwrap().signature_hex;
+//         // let public_key_string = public_key().await.unwrap().public_key_hex;
+//         // let verify_result: Result<SignatureVerificationReply, String> = verify(signature_hex.clone(), program_hash.to_string(), public_key_string.clone()).await;
+//         // sig, publickey, message
+//         return (signature.signature_hex, public_input_hash, output);
+//     }
+// }
 
 
 #[ic_cdk::query]
